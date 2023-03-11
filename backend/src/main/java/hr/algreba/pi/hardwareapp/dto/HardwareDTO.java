4ce758f192da49b89acf45edd2429d229faee8b6
MarioSomodi <mario0.0somodi@gmail.com>
2023-03-11 16:55:10 +0100

    Added CRUD functionality to the javafx application, extended the dto in the backend

--- backend/src/main/java/hr/algreba/pi/hardwareapp/dto/HardwareDTO.java
+++ backend/src/main/java/hr/algreba/pi/hardwareapp/dto/HardwareDTO.java
@@ -1,6 +1,7 @@
 package hr.algreba.pi.hardwareapp.dto;
 
 import hr.algreba.pi.hardwareapp.domain.Hardware;
+import hr.algreba.pi.hardwareapp.domain.Type;
 
 import java.math.BigDecimal;
 
@@ -8,17 +9,31 @@ public class HardwareDTO {
     private final String name;
     private final BigDecimal price;
     private final String code;
+    private final Type type;
+    private final Long stock;
 
     public HardwareDTO(Hardware hardware) {
         this.name = hardware.getName();
         this.price = hardware.getPrice();
         this.code = hardware.getCode();
+        this.type = hardware.getType();
+        this.stock = hardware.getStock();
     }
 
     public HardwareDTO(String code, Hardware hardware) {
         this.name = hardware.getName();
         this.price = hardware.getPrice();
+        this.type = hardware.getType();
+        this.stock = hardware.getStock();
         this.code = code;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Long getStock() {
+        return stock;
     }
 
     public String getName() {
@@ -38,6 +53,8 @@ public class HardwareDTO {
         return "HardwareDTO{" +
                 "name='" + name + '\'' +
                 ", price=" + price +
+                ", stock=" + stock +
+                ", type=" + type +
                 ", code='" + code + '\'' +
                 '}';
     }
--- frontend/src/main/java/com/example/demo/HelloController.java
+++ frontend/src/main/java/com/example/demo/HelloController.java
@@ -1,43 +1,51 @@
 package com.example.demo;
 
 import hr.algebra.dujmovic.confapp.model.Hardware;
+import hr.algebra.dujmovic.confapp.model.Type;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
+import java.math.BigDecimal;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
+import java.util.function.UnaryOperator;
+import java.util.regex.Pattern;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 public class HelloController implements Initializable {
-    @FXML
-    private Label welcomeText;
 
     @FXML
     private TableView<Hardware> hardwareTable;
-
+    @FXML
+    private TextField hardwareCodeTextField;
+    @FXML
+    private TextField hardwareNameTextField;
+    @FXML
+    private TextField hardwareStockTextField;
+    @FXML
+    private TextField hardwarePriceTextField;
+    @FXML
+    private ComboBox<Type> typeComboBox;
     @FXML
     private TableColumn<Hardware, String> nameColumn;
-
     @FXML
     private TableColumn<Hardware, String> priceColumn;
-
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TableColumn<Hardware, Type> typeColumn;
+    @FXML
+    private TableColumn<Hardware, Long> stockColumn;
+    @FXML
+    private TableColumn<Hardware, String> codeColumn;
 
+    private <T> ResponseEntity<T> getResponseFromAPI(String restEndpointUrl, GenericClass<T> classToMap, HttpMethod method, Hardware... hardware) {
         List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
         MappingJackson2HttpMessageConverter converter =
                 new MappingJackson2HttpMessageConverter();
@@ -47,29 +55,97 @@ public class HelloController implements Initializable {
         messageConverters.add(converter);
 
         RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<T> hardwareArrayResponse = new ResponseEntity<T>(HttpStatus.OK);
         restTemplate.setMessageConverters(messageConverters);
-
-        String restEndpointUrl = "http://localhost:8081/hardware";
-
-        ResponseEntity<Hardware[]> hardwareArrayResponse =
-                restTemplate.getForEntity(restEndpointUrl, Hardware[].class);
-
-        for(Hardware hardware : hardwareArrayResponse.getBody()) {
-            System.out.println("Hardware name: " + hardware.getName());
-            System.out.println("Hardware type: " + hardware.getPrice());
+        switch(method){
+            case GET:{
+                hardwareArrayResponse = restTemplate.getForEntity(restEndpointUrl, classToMap.getMyType());
+                break;
+            }
+            case DELETE:{
+                restTemplate.delete(restEndpointUrl);
+                break;
+            }
+            case PUT, POST:{
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                HttpEntity<Hardware> requestEntity = new HttpEntity<Hardware>(hardware[0], headers);
+                hardwareArrayResponse = restTemplate.exchange(restEndpointUrl, method, requestEntity, classToMap.getMyType());
+                break;
+            }
         }
+        return hardwareArrayResponse;
+    }
+
+    private Hardware getHardwareObjectFromInputs(){
+        return new Hardware(
+                null,
+                hardwareNameTextField.getText(),
+                typeComboBox.getValue(),
+                hardwareCodeTextField.getText(),
+                Long.parseLong(hardwareStockTextField.getText()),
+                new BigDecimal(hardwarePriceTextField.getText())
+        );
+    }
+
+    @FXML
+    protected void onGetAllButtonClick() {
+        GenericClass<Hardware[]> classToMapTo = new GenericClass<Hardware[]>(Hardware[].class);
+        ResponseEntity<Hardware[]> apiResponse = getResponseFromAPI("http://localhost:8081/hardware", classToMapTo, HttpMethod.GET);
 
         ObservableList<Hardware> hardwareList = FXCollections.observableArrayList();
-        Hardware[] hardwareArray = hardwareArrayResponse.getBody();
-        List<Hardware> hardwares = Arrays.asList(hardwareArray);
-        hardwareList.addAll(hardwares);
+        List<Hardware> hardwareObjects = Arrays.asList(apiResponse.getBody());
+        hardwareList.addAll(hardwareObjects);
 
         hardwareTable.setItems(hardwareList);
     }
 
+    @FXML
+    protected void onGetByCodeButtonClick() {
+        GenericClass<Hardware> classToMapTo = new GenericClass<Hardware>(Hardware.class);
+        ResponseEntity<Hardware> apiResponse = getResponseFromAPI("http://localhost:8081/hardware/" + hardwareCodeTextField.getText(), classToMapTo, HttpMethod.GET);
+
+        ObservableList<Hardware> hardwareList = FXCollections.observableArrayList();
+        List<Hardware> hardwareObjects = Arrays.asList(apiResponse.getBody());
+        hardwareList.addAll(hardwareObjects);
+
+        hardwareTable.setItems(hardwareList);
+    }
+
+    @FXML
+    protected void onDeleteButtonClick() {
+        GenericClass<String> classToMapTo = new GenericClass<String>(String.class);
+        ResponseEntity<String> apiResponse = getResponseFromAPI("http://localhost:8081/hardware/" + hardwareCodeTextField.getText(), classToMapTo, HttpMethod.DELETE);
+
+        onGetAllButtonClick();
+    }
+
+    @FXML
+    protected void onUpdateHardwareButtonClick() {
+        GenericClass<Hardware> classToMapTo = new GenericClass<Hardware>(Hardware.class);
+        ResponseEntity<Hardware> apiResponse = getResponseFromAPI("http://localhost:8081/hardware/" + hardwareCodeTextField.getText(), classToMapTo, HttpMethod.PUT, getHardwareObjectFromInputs());
+
+        onGetAllButtonClick();
+    }
+
+    @FXML
+    protected void onCreateHardwareButtonClick() {
+        GenericClass<Hardware> classToMapTo = new GenericClass<Hardware>(Hardware.class);
+        ResponseEntity<Hardware> apiResponse = getResponseFromAPI("http://localhost:8081/hardware/", classToMapTo, HttpMethod.POST, getHardwareObjectFromInputs());
+
+        onGetAllButtonClick();
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        Pattern charLimit = Pattern.compile(".{0,7}");
+        TextFormatter charLimitFormatter = new TextFormatter((UnaryOperator<TextFormatter.Change>) change -> charLimit.matcher(change.getControlNewText()).matches() ? change : null);
+        hardwareCodeTextField.setTextFormatter(charLimitFormatter);
+        codeColumn.setCellValueFactory(new PropertyValueFactory<Hardware, String>("code"));
         nameColumn.setCellValueFactory(new PropertyValueFactory<Hardware, String>("name"));
         priceColumn.setCellValueFactory(new PropertyValueFactory<Hardware, String>("price"));
+        typeColumn.setCellValueFactory(new PropertyValueFactory<Hardware, Type>("type"));
+        stockColumn.setCellValueFactory(new PropertyValueFactory<Hardware, Long>("stock"));
+        typeComboBox.getItems().setAll(Type.values());
     }
 }
